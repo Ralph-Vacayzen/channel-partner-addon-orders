31c36b1642a9a71060ea4b5d0c196c4de0575a50
Ralph McCracken <ralph@mccracken.work>
2024-10-02 11:07:35 -0500

    CPAO-1 user is prompted to upload required file

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ st.caption('VACAYZEN')
 st.title('Channel Partner Add-on Orders')
 st.info('Coorelating add-on orders to channel partner homes.')
 
+partner_properties = st.file_uploader('Partner Properties','CSV')
+
+if partner_properties:
+    pp = pd.read_csv(partner_properties, index_col=False)
+
+    partner_column = 'PARTNER'
+    order_column   = 'ORDER #'
+
+    if not 'PARTNER' in pp.columns: partner_column = st.selectbox('Partner Column', options=pp.columns)
+    if not 'ORDER #' in pp.columns: order_column   = st.selectbox('Order Column',   options=pp.columns)
+
+    ppdf = pp[[partner_column, order_column]].drop_duplicates()
