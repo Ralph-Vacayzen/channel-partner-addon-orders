0573e815e5950d5e3109ea4a1849049f684db3b7
Ralph McCracken <ralph@mccracken.work>
2024-10-02 09:14:15 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title='Channel Partner Add-on Orders', page_icon='⚙️', layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+
+st.caption('VACAYZEN')
+st.title('Channel Partner Add-on Orders')
+st.info('Coorelating add-on orders to channel partner homes.')
+
